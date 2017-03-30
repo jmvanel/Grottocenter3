5384cfee6bbac56ea6d40f2c1a985b49dd217f55
kurdty <kurdty@free.fr>
2017-03-30 10:59:09 +0200

    fix(lint): clean codebase to prepare merge from master

--- config/policies.js
+++ config/policies.js
@@ -23,98 +23,98 @@ Policy Mappings
   */
 
 module.exports.policies = {
-    /***************************************************************************
-     *                                                                          *
-     * Default policy for all controllers and actions (`true` allows public     *
-     * access)                                                                  *
-     *                                                                          *
-     ***************************************************************************/
+  /***************************************************************************
+   *                                                                          *
+   * Default policy for all controllers and actions (`true` allows public     *
+   * access)                                                                  *
+   *                                                                          *
+   ***************************************************************************/
 
+  '*': false,
+
+  IndexController: {
     '*': false,
+    'index': 'localize',
+    'map': 'localize'
+  },
 
-    IndexController: {
-        '*': false,
-        'index': 'localize',
-        'map': 'localize'
-    },
+  AuthController: {
+    'login': true,
+    'logout': 'tokenAuth'
+  },
 
-    AuthController: {
-        'login': true,
-        'logout': 'tokenAuth'
-    },
+  CaverController: {
+    '*': true,
+    'update': 'tokenAuth',
+    'destroy': 'tokenAuth'
+  },
 
-    CaverController: {
-        '*': true,
-        'update': 'tokenAuth',
-        'destroy': 'tokenAuth'
-    },
+  EntryController: {
+    '*': 'tokenAuth',
+    'find': true,
+    'findAll': true,
+    'findRandom': true
+  },
 
-    EntryController: {
-        '*': 'tokenAuth',
-        'find': true,
-        'findAll': true,
-        'findRandom': true
-    },
+  SearchController: {
+    'findAll': true,
+    'findByBounds': true
+  },
 
-    SearchController: {
-        'findAll': true,
-        'findByBounds': true
-    },
+  CaveController: {
+    '*': true
+  },
 
-    CaveController: {
-        '*': true
-    },
+  AuthorController: {
+    '*': true
+  },
 
-    AuthorController: {
-        '*': true
-    },
+  PartnerController: {
+    '*': true
+  },
 
-    PartnerController: {
-        '*': true
-    },
+  I18nController: {
+    '*': true
+  },
 
-    I18nController: {
-        '*': true
-    },
+  SwaggerController: {
+    '*': true
+  },
 
-    SwaggerController: {
-        '*': true
-    },
+  TopographyController: {
+    '*': true
+  },
 
-    TopographyController: {
-        '*': true
-    },
+  CommentController: {
+    '*': true
+  },
 
-    CommentController: {
-        '*': true
-    },
+  GrottoController: {
+    '*': true
+  },
 
-    GrottoController: {
-        '*': true
-    },
+  AdminController: {
+    '*': true
+  }
 
-    AdminController: {
-        '*': true
-    }
+  /***************************************************************************
+   *                                                                          *
+   * Here's an example of mapping some policies to run before a controller    *
+   * and its actions                                                          *
+   *                                                                          *
+   ***************************************************************************/
+  // RabbitController: {
 
-    /***************************************************************************
-     *                                                                          *
-     * Here's an example of mapping some policies to run before a controller    *
-     * and its actions                                                          *
-     *                                                                          *
-     ***************************************************************************/
-    // RabbitController: {
+  // Apply the `false` policy as the default for all of RabbitController's actions
+  // (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
+  // '*': false,
 
-    // Apply the `false` policy as the default for all of RabbitController's actions
-    // (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-    // '*': false,
+  // For the action `nurture`, apply the 'isRabbitMother' policy
+  // (this overrides `false` above)
+  // nurture	: 'isRabbitMother',
 
-    // For the action `nurture`, apply the 'isRabbitMother' policy
-    // (this overrides `false` above)
-    // nurture	: 'isRabbitMother',
-
-    // Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-    // before letting any users feed our rabbits
-    // feed : ['isNiceToAnimals', 'hasRabbitFood']
-    // }
+  // Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
+  // before letting any users feed our rabbits
+  // feed : ['isNiceToAnimals', 'hasRabbitFood']
+  // }
 };
