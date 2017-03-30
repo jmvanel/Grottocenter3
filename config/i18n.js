6e407126b1d50b71b6c886ba9bafed9f550d5512
kurdty <kurdty@free.fr>
2017-03-30 23:34:47 +0200

    feat(i18n): reorder languages by isocode asc

--- config/i18n.js
+++ config/i18n.js
@@ -22,21 +22,21 @@ module.exports.i18n = {
    *                                                                          *
    ***************************************************************************/
 
-  locales: ['en', 'es', 'fr', 'de', 'it', 'bg', 'ca', 'nl', 'ar', 'id', 'el'],
+  locales: ['ar', 'bg', 'ca', 'de', 'el', 'en', 'es', 'fr', 'id', 'it', 'nl'],
 
   localesText: {
+    'ar': 'عربية',
+    'bg': 'Български',
+    'ca': 'Català',
+    'de': 'Deutsch',
+    'el': 'Ελληνικός',
     'en': 'English',
     'es': 'Español',
     'fr': 'Français',
-    'de': 'Deutsch',
-    'it': 'Italiano',
-    'bg': 'Български',
-    'ca': 'Català',
-    'nl': 'Nederlands',
-    'ar': 'عربية',
     'id': 'Indonesia',
-    'el': 'Ελληνικός',
-    'ja': '日本語'
+    'it': 'Italiano',
+    'ja': '日本語',
+    'nl': 'Nederlands'
   },
 
   /****************************************************************************
